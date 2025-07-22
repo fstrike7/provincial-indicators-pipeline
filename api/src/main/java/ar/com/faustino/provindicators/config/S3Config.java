056af8f037a3cabc185678c68aa5e03b100697e7
Faus <cuentadefaustinob@gmail.com>
2025-07-22 10:41:48 -0300

    test: add real Spring Boot and PySpark tests + test profile

--- api/src/main/java/ar/com/faustino/provindicators/config/S3Config.java
+++ api/src/main/java/ar/com/faustino/provindicators/config/S3Config.java
@@ -3,6 +3,7 @@ package ar.com.faustino.provindicators.config;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3Configuration;
 
 @Configuration
+@ConditionalOnProperty(name = "aws.s3.enabled", havingValue = "true", matchIfMissing = true)
 public class S3Config {
     @Value("${aws.s3.endpoint}")
     private String s3Endpoint;
